59e270d8bf518ac8e72242507377bbad77ea0530
Amanda Westort <awestort@fb.com>
2017-06-28 14:46:09 -0700

    refreshworks

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -2,6 +2,7 @@ package com.codepath.apps.restclienttemplate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -33,10 +35,12 @@ public class TimelineActivity extends AppCompatActivity {
     private final int REQUEST_CODE = 20;
     Tweet newtweet;
     private SwipeRefreshLayout refreshLayout;
+    MenuItem miActionProgressItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_timeline);
         client = TwitterApp.getRestClient();
 
@@ -51,150 +55,38 @@ public class TimelineActivity extends AppCompatActivity {
         // set the adapter
         rvTweets.setAdapter(tweetAdapter);
         populateTimeline();
+
         refreshLayout = (SwipeRefreshLayout) findViewById(swipeContainer);
         refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-                // Your code to refresh the list here.
-                // Make sure you call swipeContainer.setRefreshing(false)
-                // once the network request has completed successfully.
+                showProgressBar();
                 tweetAdapter.clear();
-                //fetchTimelineAsync(0);
                 populateTimeline();
                 refreshLayout.setRefreshing(false);
+                hideProgressBar();
             }
         });
-
-        // Configure the refreshing colors
     }
-//    public void fetchTimelineAsync(int page) {
-//        // Send the network request to fetch the updated data
-//        // `client` here is an instance of Android Async HTTP
-//        // getHomeTimeline is an example endpoint.
-//
-//        client.getHomeTimeline(0, new JsonHttpResponseHandler() {
-//            public void onSuccess(JSONArray json) {
-//                // Remember to CLEAR OUT old items before appending in the new ones
-//
-//                // ...the data has come back, add new items to your adapter...
-//                TweetAdapter.addAll(json);
-//                // Now we call setRefreshing(false) to signal refresh has finished
-//
-//            }
-//
-//            public void onFailure(Throwable e) {
-//                Log.d("DEBUG", "Fetch timeline error: " + e.toString());
-//            }
-//        });
-//    }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // Store instance of the menu item containing progress
+        miActionProgressItem = menu.findItem(R.id.miActionProgress);
+         //Extract the action-view from the menu item
+        ProgressBar v = (ProgressBar) MenuItemCompat.getActionView(miActionProgressItem);
+        // Return to finish
+        return super.onPrepareOptionsMenu(menu);
+    } 
+    public void showProgressBar() {
+        // Show progress item
+        miActionProgressItem.setVisible(true);
+    }
 
-
-
-    //initSwipeOptions();
-        // Setup refresh listener which triggers new data loading
-
-//    private void initSwipeOptions() {
-//        refreshLayout.setOnRefreshListener(this);
-//        setAppearance();
-//        disableSwipe();
-//    }
-//
-//    private void setAppearance() {
-//        refreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
-//                android.R.color.holo_green_light,
-//                android.R.color.holo_orange_light,
-//                android.R.color.holo_red_light);
-//    }
-//
-//    /**
-//     * It shows the SwipeRefreshLayout progress
-//     */
-//    public void showSwipeProgress() {
-//        refreshLayout.setRefreshing(true);
-//    }
-//
-//    /**
-//     * It shows the SwipeRefreshLayout progress
-//     */
-//    public void hideSwipeProgress() {
-//        refreshLayout.setRefreshing(false);
-//    }
-//
-//    /**
-//     * Enables swipe gesture
-//     */
-//    public void enableSwipe() {
-//        refreshLayout.setEnabled(true);
-//    }
-//
-//    /**
-//     * Disables swipe gesture. It prevents manual gestures but keeps the option tu show
-//     * refreshing programatically.
-//     */
-//    public void disableSwipe() {
-//        refreshLayout.setEnabled(false);
-//    }
-//
-//    /**
-//     * It must be overriden by parent classes if manual swipe is enabled.
-//     */
-//    @Override public void onRefresh() {
-//        // Empty implementation
-//    }
-//    refreshLayout.setOnScrollListener(new AbsListView.OnScrollListener() {
-//        @Override
-//        public void onScrollStateChanged(AbsListView view, int scrollState) {
-//
-//        }
-//
-//        @Override
-//        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-//            int topRowVerticalPosition =
-//                    (guidesList == null || guidesList.getChildCount() == 0) ?
-//                            0 : guidesList.getChildAt(0).getTop();
-//            swipeContainer.setEnabled(firstVisibleItem == 0 && topRowVerticalPosition >= 0);
-//        }
-//    });
-//
-
-//        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-//            @Override
-//            public void onRefresh() {
-//                // Your code to refresh the list here.
-//                // Make sure you call swipeContainer.setRefreshing(false)
-//                // once the network request has completed successfully.
-//                fetchTimelineAsync(0);
-//            }
-//        });
-//        // Configure the refreshing colors
-//        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-//                android.R.color.holo_green_light,
-//                android.R.color.holo_orange_light,
-//                android.R.color.holo_red_light);
-//
-//    }
-//    public void fetchTimelineAsync(int page) {
-//        // Send the network request to fetch the updated data
-//        // `client` here is an instance of Android Async HTTP
-//        // getHomeTimeline is an example endpoint.
-//
-//        client.getHomeTimeline(0, new JsonHttpResponseHandler() {
-//            public void onSuccess(JSONArray json) {
-//                // Remember to CLEAR OUT old items before appending in the new ones
-//                tweetAdapter.clear();
-//                // ...the data has come back, add new items to your adapter...
-//                tweetAdapter.addAll(tweets);
-//                // Now we call setRefreshing(false) to signal refresh has finished
-//                swipeContainer.setRefreshing(false);
-//            }
-//
-//            public void onFailure(Throwable e) {
-//                Log.d("DEBUG", "Fetch timeline error: " + e.toString());
-//            }
-//        });
-//    }
-
+    public void hideProgressBar() {
+        // Hide progress item
+        miActionProgressItem.setVisible(false);
+    }
 
     private void populateTimeline() {
         client.getHomeTimeline(0, new JsonHttpResponseHandler(){
