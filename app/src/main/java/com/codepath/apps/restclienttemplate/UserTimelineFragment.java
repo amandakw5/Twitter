d6232cf89092ce980052a40481ef2a02551c36ef
Amanda Westort <awestort@fb.com>
2017-07-03 18:04:00 -0700

    sometabz

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/UserTimelineFragment.java
@@ -0,0 +1,10 @@
+package com.codepath.apps.restclienttemplate;
+
+import com.codepath.apps.restclienttemplate.fragments.TweetsListFragment;
+
+/**
+ * Created by awestort on 7/3/17.
+ */
+
+public class UserTimelineFragment extends TweetsListFragment {
+}
